5c2de932770a48e1b687d588e4491bb91c7abd72
Clark Grubb <clark@ubuntu.(none)>
2011-11-23 10:19:04 -0800

    Makes install.sh bash and zsh compatible.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 src_dir=`dirname $0`
 cd $src_dir
 home_dir=$1
-if [[ $# < 1 ]]; then echo "USAGE: install.sh DIR"; exit -1; fi
+if [ $# -lt 1 ]; then echo "USAGE: install.sh DIR"; exit -1; fi
 if [ ! -d $home_dir ]; then echo "NOT A DIRECTORY: " $home_dir; exit -1; fi
 rm -rf $home_dir/.emacs.d
 cp -R .emacs.d $home_dir
