2c988c21102d83e716ede1b998f5a4d1cc1e954d
Clark Grubb <clark@air-clark.local>
2011-11-25 15:39:43 -0800

    Adds a tool for manipulating the markup of wikidot tables.

---
+++ wikidot/table.rb
@@ -0,0 +1,120 @@
+#!/usr/bin/env ruby
+
+require 'getoptlong'
+require 'pp'
+
+CONTINUATION_REGEX = / _$/
+
+def bar_split(line)
+
+  inside_at_quote = false
+  a = []
+  tokens = line.scan(/\|\||@@|.+?(?=\|\||@@)|.+$/m)
+
+  s = ""
+
+  loop do
+    token = tokens.shift
+    break if token.nil?
+    case token
+    when '||'
+      if inside_at_quote
+        s += token
+      else
+        a << s
+        s = ""
+      end
+    when '@@'
+      if inside_at_quote
+        inside_at_quote = false
+      elsif tokens.include?('@@')
+        inside_at_quote = true
+      else
+        # unmatched starting @@
+      end
+      s += token
+    else
+      s += token
+    end
+  end
+
+  a << s
+
+  a
+end
+
+def parse(f)
+
+  table = []
+  columns = []
+  
+  f.each do |line|
+    
+    a = bar_split(line)
+
+    if columns.empty?
+      columns = a
+    else
+      columns[-1] += a.shift
+      columns.concat(a)
+    end
+    
+    if !CONTINUATION_REGEX.match(columns.last)
+      columns[-1].chomp!
+      table << columns
+      columns = []
+    end
+  end
+
+  unless columns.empty?
+    columns[-1].chomp!
+    table << columns
+  end
+
+  table
+end
+
+def generate(f, table)
+
+  table.each do |columns|
+    f.puts columns.join('||')
+  end  
+end
+
+def reorder(table, columns)
+
+  columns.unshift(0)
+  columns << 0
+  
+  table.map do |row|
+    columns.map { |i| row[i] }
+  end
+end
+
+def usage
+  $stderr.puts "table.rb --columns=COL1,COL2,... < INPUT"
+  exit -1
+end
+
+if $0 == __FILE__
+
+  opts = GetoptLong.new(
+                        [ '--columns', "-c", GetoptLong::REQUIRED_ARGUMENT ]
+                        )
+  
+  columns = []
+  opts.each do |opt,arg|
+    if opt == '--columns'
+      columns = arg.split(',',-1).map { |s| s.to_i }
+    end
+  end
+  
+  usage if columns.empty?
+  usage if columns.any? { |col| col.to_i < 1 }
+  
+  table = parse($stdin)
+  
+  generate($stdout, reorder(table, columns))
+
+end
+  
---
+++ wikidot/table_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require 'table'
+
+class TableTest < Test::Unit::TestCase
+  
+  def test_01
+    assert(true)
+  end
+  
+end
