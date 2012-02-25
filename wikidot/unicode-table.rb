7becc39c3b354baf948c0aee9f93491d7fdaea9c
Clark Grubb <clarkgrubb@gmail.com>
2012-02-24 22:38:20 -0800

    adds --title flag

--- wikidot/unicode-table.rb
+++ wikidot/unicode-table.rb
@@ -1,15 +1,37 @@
 #!/usr/bin/env ruby
 
-if !/^1\.9/.match(RUBY_VERSION)
+require 'getoptlong'
+
+opts = GetoptLong.new(
+  ['--html-entity', '-e',
+   GetoptLong::NO_ARGUMENT],
+  ['--title', '-t',
+   GetoptLong::REQUIRED_ARGUMENT]
+)
+
+title = nil
+html_entity = false
+
+def usage
+  raise "USAGE: unicode-table.rb [--html-entity] [--title=TITLE] START_CODE_POINT END_CODE_POINT"
+end
+
+opts.each do |opt, arg|
+  case opt
+  when '--html-entity'
+    html_entity = true
+  when '--title'
+    title = arg
+  else
+    usage
+  end
+end
+
+unless /^1\.9/.match(RUBY_VERSION)
   raise "ruby 1.9 needed for unicode support"
 end
 
-HTML_ENTITY = ( ARGV[0] == '--html-entity' ? true : false )
-ARGV.shift if HTML_ENTITY
-
-if ARGV.size != 2
-  raise "USAGE: unicode-table.rb [--html-entity] START_CODE_POINT END_CODE_POINT"
-end
+usage if ARGV.size != 2
 
 first_code_point = ARGV[0].to_i(16) / 16 * 16
 last_code_point = ARGV[1].to_i(16) / 16 * 16
@@ -17,6 +39,9 @@ last_code_point = ARGV[1].to_i(16) / 16 * 16
 def print_row(a)
       puts '||' + a.join('||') + '||' unless a.empty?
 end
+
+
+puts '||' * 17 + '~ ' + title + '||' if title
 
 header = [''] + ('0'..'9').to_a + ('A'..'F').to_a
 header.map! { |s| '~ ' + s }
@@ -30,7 +55,7 @@ row = []
     row = ['U+' + i.to_s(16)]
   end
 
-  if HTML_ENTITY
+  if html_entity
     row << '@<&#x' + i.to_s(16) + ';>@'
   else
     row << eval("\"\\u{#{i.to_s(16)}}\"")
