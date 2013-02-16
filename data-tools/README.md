9199c498f138a21a3f0105b2b5448bacc5751d20
Clark Grubb <clarkgrubb@gmail.com>
2013-02-16 11:20:06 -0800

    Add -x -e -r -z flags to csv-to-tsv.py
    These flags strip, backslash escape, replace with space, and replace with space and squeeze
    space, respectively.  The default behavior is to raise an exception if none of the flags
    are specified.

--- data-tools/README.md
+++ data-tools/README.md
@@ -1,10 +1,6 @@
 
 # TODO
 
-* csv-to-tsv.py should raise exception if encounters tab or newline in data.  Error message should indicate that there are options to strip, replace with space, or replace and squeeze space.
-* csv-to-tsv.py: strip -x
-* csv-to-tsv.py: replace -r (optional argument for replace string)
-* csv-to-tsv.py: -z squeeze (error if replace string is more than one char)
 * man pages
 * wikidot-table-to-tsv, tsv-to-wikidot-table?
 * html-table-to-tsv, tsv-to-html-table?
--- data-tools/csv-to-tsv.py
+++ data-tools/csv-to-tsv.py
@@ -8,7 +8,8 @@ import re
 import sys
 
 STRIPPER_CHARS = "\n\r\t"
-ESCAPER_REGEX = re.compile('([\n\r\t])')
+PROHIBITED_REGEX = re.compile('([\n\r\t])')
+SPACES_REGEX = re.compile(' +')
 
 
 def stripper(row):
@@ -16,7 +17,29 @@ def stripper(row):
 
 
 def escaper(row):
-    return [ESCAPER_REGEX.sub(field, '\\\1') for field in row]
+    return [PROHIBITED_REGEX.sub('\\\1', field) for field in row]
+
+
+def replacer(row):
+
+    return [PROHIBITED_REGEX.sub(' ', field)
+            for field
+            in row]
+
+
+def squeezer(row):
+
+    return [SPACES_REGEX.sub(' ', PROHIBITED_REGEX.sub(' ', field))
+            for field
+            in row]
+
+
+def detecter(row):
+    for field in row:
+        if PROHIBITED_REGEX.search(field):
+            raise ValueError('prohibited character in field: {}'.format(field))
+
+    return row
 
 
 def csv_to_tsv(input_stream,
@@ -29,8 +52,13 @@ def csv_to_tsv(input_stream,
                       delimiter=delimiter,
                       quotechar=quotechar)
 
+    lineno = 1
     for row in rows:
-        output_stream.write('\t'.join(sanitizer(row)) + '\n')
+        try:
+            output_stream.write('\t'.join(sanitizer(row)) + '\n')
+            lineno += 1
+        except ValueError as e:
+            raise ValueError('{}: at line number: {}'.format(str(e), lineno))
 
 
 if __name__ == '__main__':
@@ -43,17 +71,48 @@ if __name__ == '__main__':
     parser.add_argument('--quotechar', '-q',
                         dest='quotechar',
                         default='"')
-    parser.add_argument('--escapechar', '-e',
-                        dest='escapechar')
+    parser.add_argument('--escape', '-e',
+                        dest='escape',
+                        action='store_const',
+                        const=True)
+    parser.add_argument('--strip', '-x',
+                        dest='strip',
+                        action='store_const',
+                        const=True)
+    parser.add_argument('--replace', '-r',
+                        dest='replace',
+                        action='store_const',
+                        const=True)
+    parser.add_argument('--squeeze', '-z',
+                        dest='squeeze',
+                        action='store_const',
+                        const=True)
 
     args = parser.parse_args()
 
-    if not args.escapechar:
-        sanitizer = stripper
-    elif args.escapechar == '\\':
+    exclusive_cnt = 0
+    if args.escape:
+        exclusive_cnt += 1
+    if args.strip:
+        exclusive_cnt += 1
+    if args.replace:
+        exclusive_cnt += 1
+    if args.squeeze:
+        exclusive_cnt += 1
+    if exclusive_cnt > 1:
+        raise Exception('The flags --escape, --strip, --replace, and --squeeze'
+                        ' cannot be used together.')
+
+    if args.escape:
         sanitizer = escaper
+    elif args.strip:
+        sanitizer = stripper
+    elif args.replace:
+        sanitizer = replacer
+    elif args.squeeze:
+        sanitizer = squeezer
     else:
-        raise ValueError('unsupported escape char: {}'.format(args.escapechar))
+        sanitizer = detecter
 
     csv_to_tsv(sys.stdin,
                sys.stdout,
