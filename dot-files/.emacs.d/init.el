3af45fadbff50f77d431ba5a94318b99e9efed4d
Clark Grubb <clarkgrubb@gmail.com>
2013-05-28 13:48:10 -0700

    M-x show-tag-line

--- dot-files/.emacs.d/init.el
+++ dot-files/.emacs.d/init.el
@@ -175,6 +175,22 @@
 ;;
 (require 'latex)
 
+;; Get line a tag is defined on.  For function definitions
+;; this will often contain the signature.
+;;
+;; TODO: observe prog mode use knowledge to find end of signature
+;; also include nearby comment?  Possibly put in separate buffer
+;; if more than a certain number of lines long.  Define a suite of
+;; related functions and put in autoloaded library.
+;;
+(defun show-tag-line (tagname &optional next-p regexp-p)
+  (interactive (find-tag-interactive "Find tag: "))
+  (let* ((buf (find-tag-noselect tagname next-p regexp-p))
+         (pos (with-current-buffer buf (point)))
+         (end (with-current-buffer buf (line-end-position))))
+    (message (with-current-buffer buf (buffer-substring pos end)))))
+
+
 ;; More personal key bindings
 ;;
 (global-set-key "\C-cb" 'revert-buffer)
@@ -183,5 +199,6 @@
 (global-set-key "\C-cf" 'display-buffer-file-name)
 (global-set-key "\C-ci" 'insert-file-image)
 (global-set-key "\C-cr" 'query-replace)
+(global-set-key "\C-ct" 'show-tag-line)
 (global-set-key "\C-cv" 'clipboard-kill-ring-save)
 (global-set-key "\C-cx" 'clipboard-kill-region)
