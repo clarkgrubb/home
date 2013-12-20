c0158bf00939a3af4004aa51bc28bf5040110cfb
Clark Grubb <clarkgrubb@gmail.com>
2013-12-20 11:24:50 -0800

    add smex to emacs; byte-code compile magit.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -93,6 +93,16 @@
 (setq ido-enable-flex-matching t)
 (global-set-key "\C-x\C-b" 'ibuffer)
 
+;; Add ido to M-x
+;;
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; Old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+
 ;; Make M-x ispell use aspell
 ;;
 (dolist (path '("/usr/bin/aspell" "/usr/local/bin/aspell"))
