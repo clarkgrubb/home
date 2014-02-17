355c5969f99f77345d42360e2af2ab83fb95e90f
Clark Grubb <clarkgrubb@gmail.com>
2014-02-17 07:52:25 -0800

    rm ag

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -110,23 +110,6 @@
         (setq ispell-program-name path)
         (return))))
 
-;; A replacement for M-x grep and M-x find-grep. Adds
-;; these commands:
-;;
-;;    M-x ag
-;;    M-x ag-files
-;;    M-x ag-regexp
-;;    M-x ag-project
-;;    M-x ag-project-files
-;;    M-x ag-project-regexp
-;;
-;; Uses an external executable called 'ag'.  To install
-;;
-;;    Darwin $ brew install ag
-;;    Linux $ sudo apt-get install silversearcher-ag
-;;
-(require 'ag)
-
 ;; Adds a replacement for M-x term:
 ;;
 ;;   M-x multi-term
