c42e737d0162330e89a5f3f86dc3b8f80508fd59
Clark Grubb <clarkgrubb@gmail.com>
2014-03-23 22:34:40 -0700

    add some git info to fish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,21 @@ function fish_prompt
   else
     set face ' :( '
   end
+
   set_color red
   echo -n (uname -s)
   echo -n ':fish '
+
   set_color blue
   echo -n (prompt_pwd)
+
+  set_color green
+  if test -d .git
+    if test ! -z (which git)
+      echo -n ' git:'
+      echo -n (git branch | grep '^*' | cut -c 3-)
+    end
+  end
   set_color black
   echo -n $face
 end
