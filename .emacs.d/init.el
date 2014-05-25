c545cf97fa68c68af683f6653e44148f370c4c77
Clark Grubb <clarkgrubb@gmail.com>
2014-05-25 09:35:27 -0700

    .emacs.d/init.el: fix formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,9 +23,9 @@
 ;;
 (defun my-bell-function ()
   (unless (memq this-command
-    	'(isearch-abort abort-recursive-edit exit-minibuffer
-              keyboard-quit mwheel-scroll down up next-line previous-line
-              backward-char forward-char))
+                '(isearch-abort abort-recursive-edit exit-minibuffer
+                                keyboard-quit mwheel-scroll down up next-line
+                                previous-line backward-char forward-char))
     (ding)))
 (setq ring-bell-function 'my-bell-function)
 
