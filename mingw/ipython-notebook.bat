bae06c0d4afb6b3e61daf7886e8048927dbb65bb
Clark Grubb <clarkgrubb@gmail.com>
2014-09-29 21:09:17 -0700

    start ipython notebook w/o opening browser

--- mingw/ipython-notebook.bat
+++ mingw/ipython-notebook.bat
@@ -1,2 +1,3 @@
+start /b
 chdir C:\Users\clark\Documents\Notebooks
-C:\Users\clark\Local\src\Anaconda\Scripts\ipython.exe notebook --pylab=inline
+C:\Users\clark\Local\src\Anaconda\Scripts\ipython.exe notebook --pylab=inline --no-browser
