16fc565182ca9a23c362c0689f2d6568b76b0c82
Clark Grubb <clarkgrubb@gmail.com>
2015-01-09 08:40:49 -0800

    emacs: add pig-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -260,6 +260,9 @@
 (autoload 'php-mode "php-mode" "PHP Mode." t)
 (add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
 
+(autoload 'pig-mode "pig-mode" "Pig Mode." t)
+(add-to-list 'auto-mode-alist '("\\.pig\\'" . pig-mode))
+
 (autoload 'swift-mode "swift-mode" "Swift Mode." t)
 (add-to-list 'auto-mode-alist '("\\.swift\\'" . swift-mode))
 
