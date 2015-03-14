c7186d6d817b22477b40e94b3fb06c589db1001b
Clark Grubb <clarkgrubb@gmail.com>
2015-03-13 17:57:07 -0700

    split off ubuntu.desktop.pp from ubuntu.pp

---
+++ puppet/ubuntu.desktop.pp
@@ -0,0 +1,47 @@
+# desktop
+
+package { 'octave':
+	ensure => installed,
+}
+
+package { 'gnumeric':
+	ensure => installed,
+}
+
+package { 'r-base':
+	ensure => installed,
+}
+
+package { 'ipython-notebook':
+	ensure => installed,
+}
+
+package { 'python-sympy':
+	ensure => installed,
+}
+
+package { 'python-pandas':
+	ensure => installed,
+}
+
+package { 'texlive':
+	ensure => installed,
+}
+
+package { 'texworks':
+	ensure => installed,
+}
+
+package { 'gnome-dictionary':
+	ensure => installed,
+}
+
+package { 'inkscape':
+	ensure => installed,
+}
+
+package { 'gimp':
+	ensure => installed,
+}
+
+
--- puppet/ubuntu.pp
+++ puppet/ubuntu.pp
@@ -8,10 +8,6 @@ package { 'git':
 	ensure => installed,
 }
 
-package { 'mercurial':
-	ensure => installed,
-}
-
 package { 'zsh':
 	ensure => installed,
 }
@@ -49,52 +45,6 @@ package { 'python-pip':
 }
 
 package { 'openjdk-7-jdk':
-	ensure => installed,
-}
-
-# desktop
-
-package { 'octave':
-	ensure => installed,
-}
-
-package { 'gnumeric':
-	ensure => installed,
-}
-
-package { 'r-base':
-	ensure => installed,
-}
-
-package { 'ipython-notebook':
-	ensure => installed,
-}
-
-package { 'python-sympy':
-	ensure => installed,
-}
-
-package { 'python-pandas':
-	ensure => installed,
-}
-
-package { 'texlive':
-	ensure => installed,
-}
-
-package { 'texworks':
-	ensure => installed,
-}
-
-package { 'gnome-dictionary':
-	ensure => installed,
-}
-
-package { 'inkscape':
-	ensure => installed,
-}
-
-package { 'gimp':
 	ensure => installed,
 }
 
